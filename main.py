e4a31f9244a90f6b0b6d651216ce7c2c1cb07221
Realmirbek <mamytalievmirbek2008@gmail.com>
2025-09-22 16:18:26 +0600

    add main.py

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
-import os
 
 app = FastAPI()
 
-# Монтируем папку cerebra_local/static как /cerebra.kz
-app.mount("/cerebra.kz", StaticFiles(directory="cerebra_local/static"), name="static")
-
-# Возвращаем index.html для корневого пути
-@app.get("/")
-async def root():
-    return FileResponse(os.path.join("cerebra_local/static", "index.html"))
-
-# Поддержка пути /cerebra.kz/index.html
-@app.get("/cerebra.kz/index.html")
-async def cerebra_index():
-    return FileResponse(os.path.join("cerebra_local/static", "index.html"))
+# Раздаём статику из папки cerebra_local/static прямо с корня
+# index.html автоматически открывается по "/"
+app.mount("/", StaticFiles(directory="cerebra_local/static", html=True), name="static")
