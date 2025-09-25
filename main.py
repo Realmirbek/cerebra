44160e61b3a9ef648fa4d10e2ed25dbd573a776f
Realmirbek <mamytalievmirbek2008@gmail.com>
2025-09-25 11:26:22 +0600

    add new html0

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import os
 
 app = FastAPI()
 
-app.mount("/", StaticFiles(directory="cerebra_local/static", html=True), name="static")
+# Подключаем статику (css, js)
+app.mount("/static", StaticFiles(directory="cerebra_local/static"), name="static")
 
+# Подключаем шаблоны
+templates = Jinja2Templates(directory="templates")
+
+# Роут для главной страницы
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
